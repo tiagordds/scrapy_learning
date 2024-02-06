8a8af24ceed89c7934aa1eed80958b07402c61e1
Tiago Rodrigues <yutri.trds@gmail.com>
2024-02-06 17:01:15 -0300

    works inside a while loop

--- learning/learning/spiders/hackernews.py
+++ learning/learning/spiders/hackernews.py
@@ -10,15 +10,25 @@ class HackernewsSpider(scrapy.Spider):
     def parse(self, response):
         all_page = response.css('#hnmain')
         x = 0
-        while x < 31:
+        while x <= 28:
 
             for post_info in all_page:
                 all_title = post_info.css("span.titleline")
                 title = all_title.css("a::text")[x].get()
                 all_info = post_info.css("span.subline")
                 points = all_info.css("span.score::text")[x].get()
+                author = all_info.css("a.hnuser::text")[x].get()
+                hour = all_info.css("span.age::attr(title)")[x].get()
+
                 yield {
                     'title': title,
-                    'points': points
+                    'points': points,
+                    'author': author,
+                    'hour': hour
                 }
+
+                if x == -1:
+                    x += 31
                 x += 1
+                if x == 29:
+                    x -= 30
