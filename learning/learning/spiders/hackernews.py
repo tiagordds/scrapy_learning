61437e166120871f0d290cb1a3737f2fee485cfa
Tiago Rodrigues <yutri.trds@gmail.com>
2024-02-08 14:34:05 -0300

    add xpath
    Trying to use xpath. Works only for titlelines, not fully developed yet.

--- learning/learning/spiders/hackernews.py
+++ learning/learning/spiders/hackernews.py
@@ -8,27 +8,10 @@ class HackernewsSpider(scrapy.Spider):
     start_urls = ["https://news.ycombinator.com"]
 
     def parse(self, response):
-        all_page = response.css('#hnmain')
-        x = 0
-        while x <= 28:
+        body = response.xpath("//td")
 
-            for post_info in all_page:
-                all_title = post_info.css("span.titleline")
-                title = all_title.css("a::text")[x].get()
-                all_info = post_info.css("span.subline")
-                points = all_info.css("span.score::text")[x].get()
-                author = all_info.css("a.hnuser::text")[x].get()
-                hour = all_info.css("span.age::attr(title)")[x].get()
-
-                yield {
-                    'title': title,
-                    'points': points,
-                    'author': author,
-                    'hour': hour
-                }
-
-                if x == -1:
-                    x += 31
-                x += 1
-                if x == 29:
-                    x -= 30
+        for tr in body.xpath(".//*[contains(concat(' ', normalize-space(@class), ' '), ' titleline ')]//text()"):
+            title = tr.getall()
+            yield {
+                "title": title
+            }
