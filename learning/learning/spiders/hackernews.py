a75faded397fec58101765464b04a3d1b5545cb2
Tiago Rodrigues <yutri.trds@gmail.com>
2024-02-15 15:57:34 -0300

    fix the bug with hiring post

--- learning/learning/spiders/hackernews.py
+++ learning/learning/spiders/hackernews.py
@@ -8,19 +8,13 @@ class HackernewsSpider(scrapy.Spider):
     start_urls = ["https://news.ycombinator.com"]
 
     def parse(self, response):
-        all_body = response.xpath(
-            "//table[@id='hnmain']")
+        all_body = response.css('tr.athing')
 
-        title = all_body.css("span.titleline a::text").getall()
-        score = all_body.css("span.score::text").getall()
-        hour = all_body.css("span.age::attr(title)").getall()
-        author = all_body.css("a.hnuser::text").getall()
-
-        for title_, points_, hour_, author_ in zip(title, score, hour, author):
-
+        for info in all_body:
+            subtext = info.xpath('following-sibling::tr')[0]
             yield {
-                'title': title_,
-                'points': points_,
-                'hour': hour_,
-                'author': author_,
+                'title': info.css('span.titleline a::text').get(),
+                'score': subtext.css('span.score::text').get(),
+                'author': subtext.css('span > a.hnuser::text').get(),
+                'hour': subtext.css('span > span.age::attr(title)').get(),
             }
